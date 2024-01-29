6b75bb3d2966c3ef7bc35864d2c6599662c6e279
Soccmai <114106608+Soccmai@users.noreply.github.com>
2024-01-29 17:31:56 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,34 @@
+question_data = [
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Books",
+     "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Music",
+     "question": "In 1993, Prince changed his name to an unpronounceable symbol because he was unhappy with his "
+                 "contract with Warner Bros.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "The 2010 film 'The Social Network' is a biographical drama film about MySpace founder Tom "
+                 "Anderson.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Geography",
+     "question": "Alaska is the largest state in the United States.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "easy", "category": "Politics",
+                                       "question": "Former president Theodore Roosevelt (1900-1908)  ran for another "
+                                                   "term under the Progressive Party in 1912.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Animals",
+     "question": "The Axolotl is an amphibian that can spend its whole life in a larval state.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "Peter Molyneux was the founder of Bullfrog Productions.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Board Games",
+     "question": "&quot;PAYDAY: The Heist&quot; is a sequel to the board game &quot;Payday&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "The name of the main character of the video game &quot;The Legend of Zelda&quot;, is Zelda.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Mythology",
+     "question": "In Norse mythology, Thor once dressed as a woman.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}
+]
